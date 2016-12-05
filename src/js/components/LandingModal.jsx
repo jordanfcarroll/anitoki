23a08b8907a191c33f0bdf623b8971d826953d3c
Rannah Derrick <rcs.derrick@gmail.com>
2016-12-05 12:24:15 -0500

    landing modal styling

--- src/js/components/LandingModal.jsx
+++ src/js/components/LandingModal.jsx
@@ -9,15 +9,18 @@ var LandingModal = React.createClass({
 	render: function () {
 		return (
 			<div className="modal">
-				<div className="about-img">
-					<h2>Never Miss an Episode</h2>
-					<h4>anitoki is the best way to keep track of all the anime that's currently airing</h4>
-				</div>
-				<button className="modal-search">SEARCH SHOWS</button>
-				<div className="signup-txt">
-					<p>Want to know when a new episode is out?
-					<span> Make an account </span>
-					to receive notifications!</p>
+				<div className="modal-wrapper">
+					<button className="close-modal fa fa-times"></button>
+					<div className="about-img">
+						<h2>Never Miss an Episode</h2>
+						<h4>anitoki is the best way to keep track of all the anime that's currently airing</h4>
+					</div>
+					<button className="modal-search">SEARCH SHOWS</button>
+					<div className="signup-txt">
+						<p>Want to know when a new episode is out?
+						<span> Make an account </span>
+						to receive notifications!</p>
+					</div>
 				</div>
 			</div>
 		);
